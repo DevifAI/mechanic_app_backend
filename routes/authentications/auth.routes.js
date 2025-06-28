8bae832a7069a356ad7be10d8de62b17b1852d9b
Souvik Das <souvikkdas17@gmail.com>
2025-06-28 13:16:12 +0530

    logout route implmented with middleware

--- routes/authentications/auth.routes.js
+++ routes/authentications/auth.routes.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { changePassword, login } from "../../controllers/auth/auth.controller.js";
+import { changePassword, login, logout } from "../../controllers/auth/auth.controller.js";
+import jwtMiddleware from "../../middleware/jwtMiddleware.js";
 
 
 const router = express.Router();
 
 router.post('/login', login);
 router.post('/change/pass', changePassword);
-
+router.post('/logout', jwtMiddleware, logout);
 
 export default router;

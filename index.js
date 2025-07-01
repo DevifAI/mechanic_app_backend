685b98a8a8881fd6d78678a56a22130a755fff2f
devify2022 <2022devify@gmail.com>
2025-07-01 10:34:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -118,5 +118,5 @@ connection();
 syncModels(); // Ensure models are synced before starting the server
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
